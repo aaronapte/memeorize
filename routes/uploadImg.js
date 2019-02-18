41d15353e3719660a1983a4a78e939bb989fe004
Aaron Apte <aapte@ucsd.edu>
2019-02-17 19:35:34 -0800

    Started developing updating likes

--- routes/uploadImg.js
+++ routes/uploadImg.js
@@ -1,9 +1,11 @@
  var data = require("../data.json");
+ count = 3;
 
 exports.addFriend = function(request, response) {
  // Your code goes here
  var str = "#"+ request.query.subject;
- json = {'picture': "meme4.jpg",'caption': str};
+ count++;
+json = {'picture': "meme4.jpg",'caption': str,'postNumber': count};
  console.log("cs");
  console.log(json);
  data.memes.push(json);
